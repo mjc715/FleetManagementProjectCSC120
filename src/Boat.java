fd175e7b4a8d3e5ac3f0ff05dfa48a07c39084a8
mjc715 <mjc715@miami.edu>
2021-11-11 18:25:11 -0500

    v4 Printing boat and adding boat working

--- src/Boat.java
+++ src/Boat.java
@@ -1,19 +1,20 @@
-
+//----------------------------------------------------------------------------------------------------------------------
  public class Boat {
 
-    private String name, model;
-    private double value;
-    private int expenses, year, boatTypeInt, length;
+    private String name, model, boatTypeString;
+    private double value, expenses;
+    private int year, boatTypeInt, length;
 
 
      private static enum boatType {POWER, SAILING};
     boatType boatTypes [] = boatType.values();
-
+//----------------------------------------------------------------------------------------------------------------------
     public Boat() {
         name = "";
         value = 0;
         expenses = 0;
     }
+//----------------------------------------------------------------------------------------------------------------------
     public Boat(int boatTypeInt, String name, int year, String model, int length, double value) {
         this();
         this.name = name;
@@ -23,13 +24,10 @@
         this.model = model;
         this.year = year;
     }
+//----------------------------------------------------------------------------------------------------------------------
     public Boat(String [] boatData) {
         this();
-        if (boatData[0].equals("POWER")) {
-            boatTypeInt = 0;
-        } else {
-            boatTypeInt = 1;
-        }
+        boatTypeString = boatData[0];
         name = boatData[1];
         year = Integer.parseInt(boatData[2]);
         model = boatData[3];
@@ -37,19 +35,14 @@
         value = Double.parseDouble(boatData[5]);
 
     }
-    public double getValue() {
-        return(value);
+ //---------------------------------------------------------------------------------------------------------------------
+    public void printAll() {
+
+        System.out.printf("    %8s %-20s %-5d%-10s%2d' Paid: $%9.2f Expenses: $ %9.2f\n", boatTypeString, name, year,
+                model, length, value, expenses);
+
+        }
+//----------------------------------------------------------------------------------------------------------------------
     }
-    public String getName() {
-        return(name);
-    }
-    public int getExpenses() {
-        return(expenses);
-    }
-    public String getBoatType() {
-        return(boatTypes[boatTypeInt].toString());
-    }
-    public String toString() {
-        return("");
-    }
-}
+
+
--- src/FleetManagement.java
+++ src/FleetManagement.java
@@ -1,13 +1,52 @@
 import java.io.*;
 import java.util.*;
+//======================================================================================================================
 public class FleetManagement {
+    private static Scanner keyboard = new Scanner(System.in);
+//----------------------------------------------------------------------------------------------------------------------
     public static void main(String[] args) {
 
+        ArrayList<Boat> boatList = new ArrayList<>();
         Scanner fileScanner = null;
+
+        boolean proceed = false;
+
         if (args.length > 0) {
-            inputData(args[0]);
+           boatList = inputData(args[0]);
+        }
+        System.out.println("Welcome to Castellucci Fleet Management");
+        System.out.println("---------------------------------------");
+        System.out.println();
+        try {
+            while (!proceed) {
+                System.out.println("(P)rint, (A)dd, (R)emove, (E)xpense, or E(x)it");
+                switch (keyboard.next().charAt(0)) {
+                    case 'P':
+                        printFleet(boatList);
+                        System.out.println();
+                        break;
+                    case 'A':
+                        boatList = addBoat(boatList);
+                        break;
+                    case 'R':
+                        //removeBoat();
+                        break;
+
+                    case 'E':
+                        //boatExpense();
+                        break;
+                    case 'X':
+                        //exitAndSave();
+                        proceed = true;
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
+
+//----------------------------------------------------------------------------------------------------------------------
     private static ArrayList<Boat> inputData(String fileName) {
 
         Scanner fileScanner = null;
@@ -35,4 +74,26 @@ public class FleetManagement {
         }
         return(boatList);
     }
+//----------------------------------------------------------------------------------------------------------------------
+    private static void printFleet(ArrayList<Boat> boatList) {
+
+        int i;
+        System.out.println("Fleet report:");
+        for (i = 0; i < boatList.size(); ++i) {
+            boatList.get(i).printAll();
+        }
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+    private static ArrayList<Boat> addBoat(ArrayList<Boat> boatList) {
+        String inputLine;
+        System.out.printf("%-50s:", "Please enter the new boat CSV data");
+        inputLine = keyboard.nextLine();
+        inputLine = keyboard.nextLine();
+        String [] attributes = inputLine.split(",");
+        Boat boat = new Boat(attributes);
+        boatList.add(boat);
+        return(boatList);
+    }
 }
+//======================================================================================================================
